2185b59a7ebe6686ddc69d67ac5bd8e0c18c58cf
Murad Karimov <muradkarimov07@gmail.com>
2025-06-26 13:57:05 +0300

    created files in folder lib: get-cart-details, get-cart-item-details and cart-item-total-price

--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -1,3 +1,6 @@
 export { getAvailablePizzaSizes } from './get-available-pizza-sizes';
 export {calcTotalPizzaPrice} from './calc-total-pizza-price';
 export {getPizzaDetails} from './get-pizza-details';
+export {getCartDetails} from './get-cart-details';
+export {getCartItemDetails} from './get-cart-item-details';
+export {calcCartItemTotalPrice} from './calc-cart-item-total-price';
